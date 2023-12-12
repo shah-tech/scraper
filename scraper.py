72883a06efd8494c720ff06b4fb621442cf72253
shahjan ali <shahjanali@MacBook-Pro.local>
2023-12-12 23:45:55 +0000

    completed my code

--- scraper.py
+++ scraper.py
@@ -1,3 +1,12 @@
-import requests
-from bs4 import BeautifulSoup as bs
+import requests # importing a package we downloaded to allow us to make a get request
+from bs4 import BeautifulSoup as bs 
 
+github_user = input('Input your Github Username: ') #taking a user input
+
+url = 'https://github.com/' + github_user #the user input is a github username we concatenate the username and url here 
+
+r = requests.get(url) # we make a get request to grab the url we concatenated
+soup = bs (r.content, 'html.parser') #grab the html of the url we request to get 
+
+profile_image = soup.find('img', {'alt' : 'Avatar'})['src'] #on the url we get find these tags and it will assign to our target
+print(profile_image) # print our result 
